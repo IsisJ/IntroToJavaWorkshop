e9de0e83e513bcc6a45beb316b64a1107068cdc5
IsisJ <league@Mac-mini.local>
2016-08-15 15:02:40 -0700

    8-15-16

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Jimmy=new Robot ();
+Jimmy.move(100);
+Jimmy.hide();
+Robot Jen=new Robot ();
+Jen.setPenColor(Color.blue);
+Jen.penDown();
+for (int i = 0; i < 4; i++) {
+Jen.move(100);
+Jen.turn(90);}
 
-	
-		
-		
-	}
-}
+}}
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -4,24 +4,27 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
+	private static final int i = 0;
+
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		 Robot Tip =new Robot ();
 		// 5. Set your robot's pen to the down position
-
+Tip.penDown();
 		// 3. Set the robot to go at max speed (10)
-
+Tip.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
-
+for (int i = 0; i < 75; i++) {
 			// 7. Change the pen color to random
-	
-			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
+	Tip.setRandomPenColor();
+	// 6. Move the robot 5 times the current line number you are drawing (5*i)
+	Tip.move(5*g);
 			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
+	Tip.turn(360);
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
+	Tip.turn(45);
 			// 9. Set the pen width to i
+Tip.setPenWidth(5);}
 
 	}
 }
