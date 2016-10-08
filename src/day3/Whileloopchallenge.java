6eba487547ce4ec45ad3aced3e4ff1458658ba4d
IsisJ <league@Mac-mini-2.local>
2016-10-08 11:33:09 -0700

    WowLoop 10-8-16

---
+++ src/day3/Whileloopchallenge.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Whileloopchallenge {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		int num=-9;
+		while(num<=-4){
+		num=num+1;	
+		System.out.println(num);
+			
+		}
+		
+		
+	}
+}
---
+++ src/day3/popupquestion.java
@@ -0,0 +1,14 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class popupquestion {
+	public static void main(String[] args) {
+		int numTimes=1;
+		while(numTimes<=4){
+			numTimes=numTimes+1;
+		JOptionPane.showMessageDialog(null,"Why do seagulls fly over the sea?" );
+		JOptionPane.showMessageDialog(null,"Because if they flew over the bay, the would be called bagles");
+		}
+	}
+}
