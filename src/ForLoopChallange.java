c8ac72cd21a577482831bf248423a1a942b0dc9e
Savera <Level 2>
2016-11-05 11:37:28 -0700

    11/5/16

---
+++ src/ForLoopChallange.java
@@ -0,0 +1,12 @@
+
+public class ForLoopChallange {
+	public static void main(String[] args) {
+		for(int num=2;num<=10;num=num+2){
+			System.out.println(num);
+		}
+		for(int num2=99;num2>=94;num2=num2-1){
+			System.out.println(num2);
+		}
+	}
+
+}
---
+++ src/YearsAlive.java
@@ -0,0 +1,14 @@
+
+public class YearsAlive {
+	public static void main(String[] args) {
+		int age =0;
+		while(age<14){
+			age=age+1;
+			System.out.println(age);
+		}
+		for(int num=0;num<=14;num=num+1){
+			System.out.println(num);
+		}
+	}
+
+}
