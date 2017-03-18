0f0d6410df4d041adf355d398f3d5fd5ddf17f84
IsisJ <league@Mac-mini-3.local>
2017-03-18 11:32:24 -0700

    3-18-17

---
+++ src/code4Life.java
@@ -0,0 +1,50 @@
+
+
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class code4Life {
+	public static void main(String[] args) {
+		
+
+	/*
+	 * Ask the user how many hours they spent coding this week.
+	 * 
+	 * 1. If it's 3 or more, tell them they're a Code Ninja.
+	 * 
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+	 * 
+	 * 3. If it's more than 5, play the Batman theme song.
+	 */
+		String howManyHoursOfCode=JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+		int hoursOfCode= Integer.parseInt(howManyHoursOfCode);
+		if(hoursOfCode>5){
+			playBatmanTheme();
+		}
+		if(hoursOfCode>=3){
+			JOptionPane.showMessageDialog(null, "Your a code ninja!");
+		}
+		else if(hoursOfCode<2){
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead!");
+			
+		}
+		}
+	private static void playBatmanTheme() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/batman.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(60002);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
+
